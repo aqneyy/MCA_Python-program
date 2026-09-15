f43ff87dfe34c2d44abeb86cc4d4faa8f1b9fb32
Agney Unnithan <agneyunnithan94@gmail.com>
2026-09-15 09:20:44 +0530

    Add files via upload

---
+++ C02_8.py
@@ -0,0 +1,7 @@
+words=input("enter words: ").split()
+longest =""
+for word in words:
+    if len(word) > len(longest):
+        longest = word
+print("Longest word:", longest)
+print("Length:", len(longest))
